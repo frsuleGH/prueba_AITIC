c0b88f0df39283b7735bae75a521413fcc188e6a
Your frsuleGH <franciscosule@gmail.com>
2024-08-08 11:24:46 -0300

    commit inicial con campos personalizados

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Ventas AITIC',
+    'version': '1.0',
+    'category': 'Custom',
+    'description': """
+        AITIC
+    """,
+    'depends': ['base', 'sale'],
+    'data': [
+        'views/sale_order_views.xml'
+    ],
+    'installable': True,
+    'application': True,
+}
